96917fd0ce9e9449d1a51e95f640ab355e1e7cee
navateja <nava.teja@hikeezee.com>
2013-09-08 12:23:23 +0530

    creating rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beatbot::Application.config.secret_token = 'b23b7b9c5653db71f40d9a6e24505758d8651d28e4b849706e43a709978605668a8ac06e7eece8748920dcfd8367a2400dde69b5d0149026cb285af1edbe81ee'
